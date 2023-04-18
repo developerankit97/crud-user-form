25eab149691bd8871ea77455b0e0db592294905c
developerankit97 <ankitsh.developer@gmail.com>
2023-04-18 21:45:41 +0530

    new booking appointment app created with post get request

---
+++ main.js
@@ -0,0 +1,67 @@
+const userName = document.querySelector('#name');
+const userEmail = document.querySelector('#email');
+const userPhone = document.querySelector('#phone');
+const btn = document.querySelector('.btn');
+const list = document.querySelector('.items');
+
+btn.addEventListener('click', addUser);
+
+window.addEventListener('DOMContentLoaded', getAppointsments);
+
+function getAppointsments() {
+    axios
+        .get('https://crudcrud.com/api/bbf764bcba4c4345b7c4c9ad3e9d0ed4/appointments')
+        .then(response => {
+            console.log(response.data);
+            response.data.forEach(element => {
+                let output = `<li class="list-group-item d-flex justify-content-between align-items-center">
+                                <span>${element.name} - ${element.email} - ${element.phone}</span>
+                                <div>
+                                    <button type="submit" class="btn btn-info edit">Edit</button>
+                                    <button type="submit" class="btn btn-danger delete">Delete</button>
+                                </div>
+                            </li>`;
+                list.innerHTML += output;
+            });
+        })
+        .catch(err => console.error(err));
+}
+
+function addUser(e) {
+    e.preventDefault();
+    const name = userName.value;
+    const email = userEmail.value;
+    const phone = userPhone.value;
+    if (name && email && phone) {
+        let user = {
+            "name": name,
+            "email": email,
+            "phone": phone,
+        };
+        console.log(JSON.stringify(user));
+        axios
+            .post('https://crudcrud.com/api/bbf764bcba4c4345b7c4c9ad3e9d0ed4/appointments', user)
+            .then(response => {
+                let output = `<li class="list-group-item d-flex justify-content-between align-items-center">
+                            <span>${response.data.name} - ${response.data.email} - ${response.data.phone}</span>
+                            <div>
+                                <button type="submit" class="btn btn-info edit">Edit</button>
+                                <button type="submit" class="btn btn-danger delete">Delete</button>
+                            </div>
+                        </li>`;
+                list.innerHTML = output;
+            })
+            .catch(err => console.error(err));
+        // let output = `<li class="list-group-item d-flex justify-content-between align-items-center">
+        //                     <span>${name} - ${email} - ${phone}</span>
+        //                     <div>
+        //                         <button type="submit" class="btn btn-info edit">Edit</button>
+        //                         <button type="submit" class="btn btn-danger delete">Delete</button>
+        //                     </div>
+        //                 </li>`;
+        // list.innerHTML = output;
+        userName.value = '';
+        userEmail.value = '';
+        userPhone.value = '';
+    }
+}
