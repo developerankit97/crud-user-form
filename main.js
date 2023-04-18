06dd93bea251e2fa9b4397f6b8463b3a467f60e2
developerankit97 <ankitsh.developer@gmail.com>
2023-04-18 22:23:38 +0530

    delete function added

--- main.js
+++ main.js
@@ -4,17 +4,16 @@ const userPhone = document.querySelector('#phone');
 const btn = document.querySelector('.btn');
 const list = document.querySelector('.items');
 
-btn.addEventListener('click', addUser);
-
+btn.addEventListener('click', addAppointment);
 window.addEventListener('DOMContentLoaded', getAppointsments);
+list.addEventListener('click', deleteAppointment);
 
 function getAppointsments() {
     axios
         .get('https://crudcrud.com/api/bbf764bcba4c4345b7c4c9ad3e9d0ed4/appointments')
         .then(response => {
-            console.log(response.data);
             response.data.forEach(element => {
-                let output = `<li class="list-group-item d-flex justify-content-between align-items-center">
+                let output = `<li class="list-group-item d-flex justify-content-between align-items-center" id="${element._id}">
                                 <span>${element.name} - ${element.email} - ${element.phone}</span>
                                 <div>
                                     <button type="submit" class="btn btn-info edit">Edit</button>
@@ -27,7 +26,7 @@ function getAppointsments() {
         .catch(err => console.error(err));
 }
 
-function addUser(e) {
+function addAppointment(e) {
     e.preventDefault();
     const name = userName.value;
     const email = userEmail.value;
@@ -38,30 +37,33 @@ function addUser(e) {
             "email": email,
             "phone": phone,
         };
-        console.log(JSON.stringify(user));
         axios
             .post('https://crudcrud.com/api/bbf764bcba4c4345b7c4c9ad3e9d0ed4/appointments', user)
             .then(response => {
-                let output = `<li class="list-group-item d-flex justify-content-between align-items-center">
-                            <span>${response.data.name} - ${response.data.email} - ${response.data.phone}</span>
-                            <div>
-                                <button type="submit" class="btn btn-info edit">Edit</button>
-                                <button type="submit" class="btn btn-danger delete">Delete</button>
-                            </div>
-                        </li>`;
+                let output = `<li class="list-group-item d-flex justify-content-between align-items-center" id="${response.data._id}">
+                                <span>${response.data.name} - ${response.data.email} - ${response.data.phone}</span>
+                                <div>
+                                    <button type="submit" class="btn btn-info edit">Edit</button>
+                                    <button type="submit" class="btn btn-danger delete">Delete</button>
+                                </div>
+                            </li>`;
+
                 list.innerHTML = output;
             })
             .catch(err => console.error(err));
-        // let output = `<li class="list-group-item d-flex justify-content-between align-items-center">
-        //                     <span>${name} - ${email} - ${phone}</span>
-        //                     <div>
-        //                         <button type="submit" class="btn btn-info edit">Edit</button>
-        //                         <button type="submit" class="btn btn-danger delete">Delete</button>
-        //                     </div>
-        //                 </li>`;
-        // list.innerHTML = output;
         userName.value = '';
         userEmail.value = '';
         userPhone.value = '';
     }
+}
+
+function deleteAppointment(e) {
+    const item = e.target.parentElement.parentElement;
+    const id = item.getAttribute('id');
+    axios
+        .delete(`https://crudcrud.com/api/bbf764bcba4c4345b7c4c9ad3e9d0ed4/appointments/${id}`)
+        .then(response => {
+            item.remove();
+        })
+        .catch(err => console.error(err));
 }
